7a48310e89596c55eb026b59b2629a0d4a2b5ac6
Eshitayasmin <eshita.emc2k17@gmail.com>
2022-04-05 12:35:19 +0600

    line-chart add

---
+++ src/Components/Blogs/Blogs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <div>
+                <p>Question: What is a purpose of Context Api?</p>
+                <p>Answer:</p>
+                <p>In React application data is passed between two components via props. Prop-drilling is a required if data needs to be sent from one component to another.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Blogs;
